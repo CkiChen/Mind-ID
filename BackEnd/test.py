bccff0d5e6fab82dc5fc0f09392fc7214c90febe
Jatin Bhatnagar <jatinbhatnagar05@gmail.com>
2020-04-25 11:03:40 +0530

    File I/O errors fixed,check GUI.py

---
+++ BackEnd/test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 25 10:28:40 2020
+
+@author: jatin
+"""
+
+import numpy as np
+
+db = np.load('Res/dataset.npy',allow_pickle='TRUE')
+
+
+
+x = np.load('Res/Users.npy',allow_pickle='TRUE').item()
+
+
+
+
+   
+ 
